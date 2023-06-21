9f8ac69ca9c81235bb5eb53f9016c9adfa1cfbcb
BigONchik <128064684+BigONchik@users.noreply.github.com>
2023-06-21 18:49:14 +0600

    nedo navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,55 +7,22 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
-import { Link, useSearchParams } from "react-router-dom";
-import SearchIcon from "@mui/icons-material/Search";
-import InputBase from "@mui/material/InputBase";
-import { TextField } from "@mui/material";
-import { styled } from "@mui/system";
-import { useCart } from "../../contexts/CartContextProvider";
-import { getCountProductsInCart } from "../../helpers/functions";
-import { useAuth } from "../../contexts/AuthContextProvider";
-import { ADMIN } from "../../helpers/consts";
-import { useState } from "react";
-import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const pages = [
   { name: "Главная", link: "/", id: 1 },
-  { name: "Каталог", link: "/products", id: 2 },
-  // { name: "ADMIN", link: "/admin", id: 3 },
+  { name: "Новые игры", link: "/products", id: 2 },
+  { name: "Репаки от Игрухи", link: "/admin", id: 3 },
+  { name: "По сети", link: "/admin", id: 3 },
+  { name: "Заказать игру", link: "/admin", id: 3 },
+  { name: "Поддежать", link: "/admin", id: 3 },
+  { name: "ADMIN", link: "/admin", id: 3 },
 ];
-const settings = ["Profile", "Account", "Dashboard"];
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function Navbar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -76,61 +43,29 @@ function Navbar() {
     setAnchorElUser(null);
   };
 
-  const [count, setCount] = React.useState(0);
-
-  const { addProductToCart } = useCart();
-
-  React.useEffect(() => {
-    setCount(getCountProductsInCart());
-  }, [addProductToCart]);
-
-  const {
-    handleLogout,
-    user: { email },
-  } = useAuth();
-
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [search, setSearch] = useState(searchParams.get("q") || "");
-
-  useEffect(() => {
-    setSearchParams({ q: search });
-  }, [search]);
-
   return (
-    <AppBar position="static" sx={{ backgroundColor: "#222" }}>
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: "#171a21", height: "100px" }}
+    >
       <Container
         maxWidth="xl"
         sx={{
-          backgroundColor: "#222",
-          height: "85px",
           display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
+          justifyContent: "center",
+          margin: "0",
+          width: "100%",
         }}
       >
-        <Toolbar disableGutters sx={{ width: "100%" }}>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            <img
-              src="https://www.transparentpng.com/thumb/puma-logo/It9NZf-puma-logo-transparent.png"
-              alt=""
-              width="40px"
-              className="puma_icon"
-            />
-          </Typography>
+        <Toolbar
+          disableGutters
+          sx={{
+            height: "100px",
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
@@ -142,6 +77,8 @@ function Navbar() {
             >
               <MenuIcon />
             </IconButton>
+          </Box>
+          <Box>
             <Menu
               id="menu-appbar"
               anchorEl={anchorElNav}
@@ -167,148 +104,61 @@ function Navbar() {
                   </MenuItem>
                 </Link>
               ))}
-              {email === ADMIN ? (
-                <Link to="/admin">
-                  <MenuItem onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">ADMIN</Typography>
-                  </MenuItem>
-                </Link>
-              ) : null}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Link key={page.id} to={page.link}>
                 <Button
                   onClick={handleCloseNavMenu}
-                  sx={{
-                    my: 2,
-                    color: "white",
-                    display: "block",
-                    fontSize: "18px",
-                    color: "gray",
-                    "&:hover": {
-                      color: "white",
-                    },
-                  }}
+                  sx={{ my: 2, color: "white", display: "block" }}
                 >
                   {page.name}
                 </Button>
               </Link>
             ))}
-            {email === ADMIN ? (
-              <Link to="/admin">
-                <Button
-                  onClick={handleCloseNavMenu}
-                  sx={{
-                    my: 2,
-                    color: "white",
-                    display: "block",
-                    fontSize: "18px",
-                    color: "gray",
-                    "&:hover": {
-                      color: "white",
-                    },
-                  }}
-                >
-                  ADMIN
-                </Button>
-              </Link>
-            ) : null}
           </Box>
-          <Box
-            sx={{
-              border: "1px solid",
-              display: "flex",
-              alignItems: "center",
-              width: "300px",
-              height: "40px",
-            }}
-          >
-            <SearchIcon sx={{ marginLeft: "10px", marginRight: "10px" }} />
-
-            <TextField
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              fullWidth
-              label="search..."
-              variant="standard"
-            />
-          </Box>
-          <Box></Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton
-                onClick={handleOpenUserMenu}
-                sx={{ p: 0, marginLeft: "10px" }}
-              >
-                <img
-                  src="https://cdn3.iconfinder.com/data/icons/2px-stroke-simple-line/24/person-key-512.png"
-                  alt=""
-                  width="45px"
-                />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
+          <Tooltip title="Open settings">
+            <IconButton
+              onClick={handleOpenUserMenu}
+              sx={{
+                p: 0,
+                display: "flex",
+                justifyContent: "flex-end",
+                width: "8%",
+                marginLeft: "28%",
               }}
-              Mounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
             >
-              {email ? (
-                <MenuItem>
-                  <Typography textAlign="center">hello, {email}!</Typography>
-                </MenuItem>
-              ) : null}
-
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-              <MenuItem
-                onClick={() => {
-                  handleCloseUserMenu();
-                  handleLogout();
-                }}
-              >
-                <Typography textAlign="center">Logout</Typography>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">{setting}</Typography>
               </MenuItem>
-            </Menu>
-          </Box>
-          <Link to="/cart"></Link>
+            ))}
+          </Menu>
         </Toolbar>
       </Container>
     </AppBar>
   );
 }
+
 export default Navbar;
